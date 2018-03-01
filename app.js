ef638a7c8bf9412f97cb5765a78bad4993450032
joedan138 <joedan138@gmail.com>
2018-02-28 21:22:10 -0700

    rough draft 3.0 - all solved except for the date calculator and total paid column

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// initializing firebase
 var config = {
     apiKey: "AIzaSyA571n6TcSLjPftXjBI9zGxCXRkCtVMpeM",
     authDomain: "googligansclassproject1.firebaseapp.com",
@@ -5,22 +6,61 @@ var config = {
     storageBucket: "",
 };
 
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+
+// date calculator function
+
+
+
+
+var employee = "";
+var role =  "";
+var startDate = "";
+var monthlyRate =  "";
+
 $('#submitButton').on('click', function () {
+    // stopping the submit button from refreshing the page
     event.preventDefault();
+    // inputs from the form
     employee = $("#employeeName").val().trim();
     role = $("#role").val().trim();
     startDate = $("#startDate").val().trim();
     monthlyRate = $("#monthlyRate").val().trim();
 
-    newRow = ("<tr>" +
-        "<td>" + employee + "</td>" +
-        "<td>" + role + "</td>" +
-        "<td>" + startDate + "</td>" +
-        "<td>" + "Months Worked" + "</td>" +
-        "<td>" + monthlyRate + "</td>" +
-        "<td>" + "Total Billed" + "</td>" +
-        "</tr>");
-    $('#currentEmployees').append(newRow);
+    database.ref().push({
+        employee: employee,
+        role: role,
+        startDate: startDate,
+        monthlyRate: monthlyRate,
+
+    });
+
+    $("#employeeName").val("");
+    $("#role").val("");
+    $("#startDate").val("");
+    $("#monthlyRate").val("");
 });
 
-// inputs from the form
+// Retrieve new posts as they are added to our database
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+    var newPost = childSnapshot.val();
+    var employeeFB = newPost.employee;
+    var roleFB = newPost.role;
+    var startDateFB = newPost.startDate;
+    var monthlyRateFB = newPost.monthlyRate;
+
+    //  appending the date from the form into the row
+    newRow = ("<tr>" +
+    "<td>" + employeeFB + "</td>" +
+    "<td>" + roleFB + "</td>" +
+    "<td>" + startDateFB + "</td>" +
+    "<td>" + "Months Worked" + "</td>" +
+    "<td>" + monthlyRateFB + "</td>" +
+    "<td>" + "Total Billed" + "</td>" +
+    "</tr>");
+$('#currentEmployees').append(newRow);
+  });
